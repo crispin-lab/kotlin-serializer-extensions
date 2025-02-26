fb719d9125ad0a098d2ef691ecf01cd26d2383d1
crispin <h.c.shin.dev09@gmail.com>
2025-02-26 22:58:53 +0900

    :sparkles: Feat: pre commit, push logic

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,6 @@
+import org.jmailen.gradle.kotlinter.tasks.InstallPreCommitHookTask
+import org.jmailen.gradle.kotlinter.tasks.InstallPrePushHookTask
+
 val libraryVersion = "0.0.2"
 
 plugins {
@@ -37,4 +40,26 @@ tasks.jacocoTestReport {
 
 kotlin {
     jvmToolchain(21)
+}
+
+if (!rootProject.extra.has("install-git-hooks")) {
+    rootProject.extra["install-git-hooks"] = true
+
+    val preCommit: InstallPreCommitHookTask by project.rootProject.tasks.creating(
+        InstallPreCommitHookTask::class
+    ) {
+        group = "build setup"
+        description = "Installs Kotlinter Git pre-commit hook"
+    }
+
+    val prePush: InstallPrePushHookTask by project.rootProject.tasks.creating(
+        InstallPrePushHookTask::class
+    ) {
+        group = "build setup"
+        description = "Installs Kotlinter Git pre-push hook"
+    }
+
+    project.rootProject.tasks.getByName("prepareKotlinBuildScriptModel") {
+        dependsOn(preCommit, prePush)
+    }
 }
---
+++ src/test/kotlin/com/crispinlab/serializer/extensions/uuid/UUIDSerializerTest.kt
@@ -0,0 +1,4 @@
+package com.crispinlab.serializer.extensions.uuid
+
+import org.junit.jupiter.api.Assertions.*
+ class UUIDSerializerTest
